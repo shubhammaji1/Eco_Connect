c3c77f01ff6122e69bcca3559858258db5291af3
Shubham Maji <122548421+shubhammaji1@users.noreply.github.com>
2025-04-09 00:21:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,18 +8,14 @@ const multer = require('multer');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
+const PORT = process.env.PORT || 5000; 
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
 // MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/vendorVault', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log('MongoDB connection error:', err));
 
